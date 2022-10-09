8175132ada9b78638dc6dcf751e9e531877e6b04
Nijat Muhtar <nijatmn@gmail.com>
2022-10-09 16:46:29 -0400

    project template

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -1,46 +1,14 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
+
 import org.testng.annotations.Test;
-import pages.EvaluatePage;
-import pages.HomePage;
-import utils.Automations;
-import utils.BrowserUtil;
 
 public class SmokeTest {
 
 
     @Test
-    public void verifyAilabPages() {
-        BrowserUtil.openChrome();
-        Automations.init(BrowserUtil.getDriver());
-
-        HomePage homePageVar = new HomePage();
-        homePageVar.setUrl("https://ailab.acr.org");
-        homePageVar.open();
-        homePageVar.visitCreate();
-        homePageVar.visitEvaluate();
-        homePageVar.visitRun();
-
-        BrowserUtil.closeBrowser();
-    }
-
-
-    @Test
-    public void verifyUserCanEvaluatePCModel() {
-       BrowserUtil.openChrome();
-       Automations.init(BrowserUtil.getDriver());
-
-       HomePage homePageVar = new HomePage();
-       homePageVar.setUrl("https://ailab.acr.org");
-       homePageVar.open();
-       homePageVar.visitEvaluate();
-
-       EvaluatePage evaluatePageVar = new EvaluatePage();
-       evaluatePageVar.evaluatePCModel();
-
-
-       BrowserUtil.closeBrowser();
+    public void testCase1() {
+        System.out.println("Verifying test case...");
     }
 
 }//end::class
